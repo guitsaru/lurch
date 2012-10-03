2ad0de3d4ecaf4ebbd6edff6e82566d9b1edb55b
Matt Pruitt <guitsaru@gmail.com>
2012-10-03 14:31:01 -0700

    /projects/id/builds

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Lurch::Application.routes.draw do
   resources :github,  :only => :create
   resources :jenkins, :only => :create
   resources :settings
-  resources :projects
+  resources :projects do
+    resources :builds
+  end
   resources :builds
 
   root :to => 'projects#index'
