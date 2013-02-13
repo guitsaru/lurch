a23ab4c51eace52e1538f94b3673b6c5deb187a4
Matt Pruitt <guitsaru@gmail.com>
2013-02-12 22:56:33 -0800

    Reduce complexity of settings controller.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,33 +3,18 @@ class SettingsController < ApplicationController
   # GET /settings.json
   def index
     @settings = Setting.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @settings }
-    end
   end
 
   # GET /settings/1
   # GET /settings/1.json
   def show
     @setting = Setting.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @setting }
-    end
   end
 
   # GET /settings/new
   # GET /settings/new.json
   def new
     @setting = Setting.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @setting }
-    end
   end
 
   # GET /settings/1/edit
@@ -42,14 +27,10 @@ class SettingsController < ApplicationController
   def create
     @setting = Setting.new(params[:setting])
 
-    respond_to do |format|
-      if @setting.save
-        format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
-        format.json { render json: @setting, status: :created, location: @setting }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @setting.errors, status: :unprocessable_entity }
-      end
+    if @setting.save
+      redirect_to(@setting, :notice => 'Setting was successfully created.')
+    else
+      render(:action => 'new')
     end
   end
 
@@ -58,14 +39,10 @@ class SettingsController < ApplicationController
   def update
     @setting = Setting.find(params[:id])
 
-    respond_to do |format|
-      if @setting.update_attributes(params[:setting])
-        format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @setting.errors, status: :unprocessable_entity }
-      end
+    if @setting.update_attributes(params[:setting])
+      redirect_to(@setting, :notice => 'Setting was successfully updated.')
+    else
+      render(:action => 'edit')
     end
   end
 
@@ -75,9 +52,6 @@ class SettingsController < ApplicationController
     @setting = Setting.find(params[:id])
     @setting.destroy
 
-    respond_to do |format|
-      format.html { redirect_to settings_url }
-      format.json { head :no_content }
-    end
+    redirect_to(settings_url)
   end
 end
